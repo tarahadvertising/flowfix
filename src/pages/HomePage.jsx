b66cd64e5008831ad66f55a426e81fb0683a20f1
Farhananoushadpp <Farhananoushadpp@users.noreply.github.com>
2026-06-09 14:33:16 +0400

    Remove Trust section - eliminate stats from website

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import MainLayout from "../layouts/MainLayout.jsx";
 import Hero from "../sections/Hero.jsx";
-import Trust from "../sections/Trust.jsx";
 import Highlights from "../sections/Highlights.jsx";
 import About from "../sections/About.jsx";
 import Services from "../sections/Services.jsx";
@@ -10,12 +9,10 @@ import Process from "../sections/Process.jsx";
 import CTA from "../sections/CTA.jsx";
 import Contact from "../sections/Contact.jsx";
 import ScrollToTop from "../components/ScrollToTop.jsx";
-
 export default function HomePage() {
   return (
     <MainLayout>
       <Hero />
-      <Trust />
       <Highlights />
       <Services />
       <About />
